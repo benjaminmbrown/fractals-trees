035407d0486acbada70e91b24a9ed71dbcdf91dd
Benjamin M. Brown <benjaminbb@gmail.com>
2016-09-19 22:33:05 -0500

    more fractal tree work

--- sketch.js
+++ sketch.js
@@ -1,51 +1,43 @@
 var fractal;
+var theta;
 
 function setup() {
-
-    createCanvas(1200, 900);
+    createCanvas(600, 400);
     setFrameRate(1);
- 	newTree()
-
 }
-
-
 
 function draw() {
-    noLoop();
+    background(51);
+    theta = map(mouseX, 0, width, 0, PI / 2);
+    translate(width / 2, height);
+    stroke(255);
+    branch(120);
 }
 
-function mousePressed (){
-	newTree();
+function branch(length) {
+
+    //var thickness = map(length, 2, 120, 1, 5);
+    strokeWeight(2);
+    line(0, 0, 0, -length);
+    //move to end of branch
+
+    translate(0, -length);
+
+    length += 0.665;
+
+    if (length > 2) {
+
+        //Right branch
+        push(); //save curren state
+        rotate(theta);
+        branch(length);
+        pop(); //restor prev state
+
+        //Left Branch
+        push();
+        rotate(-theta);
+        branch(length);
+        pop();
+    }
+
 }
-function newTree(){
-	background(51);
-	stroke(255);
-	push();
-	translate(width/2,height);
-	branch(120);
-	pop();
-}
- 
- function branch(length){
- //	var thickness = map(length,2,120,1,5);
- 	strokeWeight(3);
- 	line(0,0,0,-length);
-
- 	//move to end of branch
- 	translate(0,-length);
-
- 	length += 0.135;
-
- 	//exit
- 	if(length>2){
- 		var n = Math.floor(random(1,4));
- 		for(var i = 0; i<n ; i++){
- 			var theta = random(-PI/3, PI/3);//random angle
- 			push();//save state before rotate
- 			rotate(theta);//rotate it
- 			branch(length);//keep branching
- 			pop();
- 		}
-
- 	}
- }
