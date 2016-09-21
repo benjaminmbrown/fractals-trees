2aef35f84668ca8542ce0b39b1da2101763792b9
Benjamin M. Brown <benjaminbb@gmail.com>
2016-09-20 21:07:35 -0500

    working with animated tree

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # fractals-trees
 Trees as fractals
-
+# fractal tree reacting to mouse move
+<img src="https://media.giphy.com/media/l0MYRT5cVjCAn2tQk/giphy.gif"/>
 # don't understand:
 <img src="https://media.giphy.com/media/26ufdGEKhDaXGwESc/giphy.gif"/>
--- sketch.js
+++ sketch.js
@@ -1,43 +1,39 @@
-var fractal;
-var theta;
+var theta;   
 
 function setup() {
-    createCanvas(600, 400);
-    setFrameRate(1);
+  createCanvas(640, 360);
 }
 
 function draw() {
-    background(51);
-    theta = map(mouseX, 0, width, 0, PI / 2);
-    translate(width / 2, height);
-    stroke(255);
-    branch(120);
+  background(51);
+  theta = map(mouseX,0,width,0,PI/2);
+
+  translate(width/2, height);
+  stroke(255);
+  branch(120);
 }
 
-function branch(length) {
+function branch(len) {
 
-    var thickness = map(length, 2, 120, 1, 5);
-    strokeWeight(thickness);
-    line(0, 0, 0, -length);
-    //move to end of branch
+  var sw = map(len,2,120,1,10);
+  strokeWeight(sw);
+ // strokeWeight(2);
+      
+  line(0, 0, 0, -len);
 
-    translate(0, -length);
+  translate(0, -len);
 
-    length += 0.665;
+  len *= 0.66;
 
-    if (length > 2) {
+  if (len > 2) {
+    push();    
+    rotate(theta);   
+    branch(len);      
+    pop();    
 
-        //Right branch
-        push(); //save curren state
-        rotate(theta);
-        branch(length);
-        pop(); //restor prev state
-
-        //Left Branch
-        push();
-        rotate(-theta);
-        branch(length);
-        pop();
-    }
-
+    push();
+    rotate(-theta);
+    branch(len);
+    pop();
+  }
 }
